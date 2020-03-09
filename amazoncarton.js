bee6d182a2faf371d00c2bc55ad5eaa1f51fc560
sdstahlman <sdstahlman@gmail.com>
2020-03-09 10:26:54 -0400

    Added alert for amazon carton while in production

---
+++ amazoncarton.js
@@ -0,0 +1,12 @@
+//Declare $ function
+var $ = function (id) { return document.getElementById(id); };
+
+
+window.onload = function () {
+    $("amz_submit").onclick = carton_work;
+
+};
+
+var carton_work = function () {
+    window.alert("UNDER CONSTRUCTION");
+};
